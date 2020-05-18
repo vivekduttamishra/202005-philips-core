fd34b9cd1bca0a88518117205da3b26c5512e3b4
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-05-18 11:48:36 +0530

    day#6update#4

---
+++ HelloNetCore/HelloNetCore/Helpers/MiddleWares.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloNetCore.Helpers
+{
+    public static class MiddleWares
+    {
+        public static void UseMappedUrl(this IApplicationBuilder app,string url, RequestDelegate handler)
+        {
+            //This is the middleware
+            app.Use(next =>
+            {
+                //This is the middleware
+                return async context =>
+                {
+                    if (context.Request.Path.StartsWithSegments(url))
+                    {
+                        await handler(context);
+                    }
+                    else
+                    {
+                        await next(context);
+                    }
+                };
+            });
+
+        }
+
+    }
+}
--- HelloNetCore/HelloNetCore/StartupV1.cs
+++ HelloNetCore/HelloNetCore/StartupV1.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using HelloNetCore.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,7 +10,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace HelloNetCore
 {
-    public class Startup
+    public class StartupV1
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -43,23 +42,45 @@ namespace HelloNetCore
             //HandleUrl(url, action); //I want to perform given action for a given url
 
 
-            //MiddleWares.HandleUrl(app, "/time", async context =>
-
-            app.UseMappedUrl("/time", async context =>
+            //process url /time or pass to the next middleware
+            app.Use(next =>
             {
-                await Task.Delay(2500);
-                var date = DateTime.Now;
-                await context.Response.WriteAsync(date.ToLongTimeString());
+                //This is the middleware
+                return async context =>
+                {
+                    if (context.Request.Path.StartsWithSegments("/time"))
+                    {
+                        await Task.Delay(2500);
+                        var date = DateTime.Now;
+                        await context.Response.WriteAsync(date.ToLongTimeString());
+                    }
+                    else
+                    {
+                        await next(context);
+                    }
+                };
+            });
+
+            //process url /date or pass to the next middleware
+            app.Use(next =>
+            {
+                //This is the middleware
+                return async context =>
+                {
+                    if (context.Request.Path.StartsWithSegments("/date"))
+                    {
+                        await Task.Delay(1500);
+                        var date = DateTime.Now;
+                        await context.Response.WriteAsync(date.ToLongDateString());
+                    }
+                    else
+                    {
+                        await next(context);
+                    }
+                };
             });
 
 
-            app.UseMappedUrl("/date", async context =>
-            {
-                await Task.Delay(1500);
-                await context.Response.WriteAsync(DateTime.Now.ToLongDateString());
-            });
-
-            
             //handle all other request <--- fallback url
             //Middleware that always returns back
             //And *NOT* passes control to the next Middleware
